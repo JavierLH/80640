bd562d8bbd4d99205bd198f09e08962567504996
JavierLH <abislopeznande@gmail.com>
2022-11-08 20:19:11 -0600

    nuevas funciones agregadas a la practica 06

--- practica06/upload/src/test/java/mx/uv/AppTest.java
+++ practica06/upload/src/test/java/mx/uv/AppTest.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package mx.uv;
 
 import static org.junit.Assert.assertTrue;
@@ -18,3 +19,25 @@ public class AppTest
         assertTrue( true );
     }
 }
+=======
+package mx.uv;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
+>>>>>>> 4fa849a10172558140852cba74bc391cb70dead1
